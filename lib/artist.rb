e2ba609a843e39f22430b5a383be5e2809ebaea5
Student <example@example.com>
2019-12-18 11:51:12 -0800

    add test and passing code to find what artists are playing on a stage

---
+++ lib/artist.rb
@@ -0,0 +1,53 @@
+class Artist
+  attr_reader :id
+  attr_accessor :artist_name, :stage_name
+
+  def initialize(id = nil, artist_name, stage_name)
+    @id = id || @@total_rows += 1
+    @artist_name = artist_name
+    @stage_name = stage_name
+  end
+
+  def stage
+    Stage.find(self.stage_name)
+  end
+
+  def self.find_by_stage(stage_id)
+  artists = []
+  @@artists.values.each do |artist|
+    if artist.stage_name == stage_id
+      artists.push(artist)
+    end
+  end
+  artists
+end
+
+  def save
+    @@artists[self.id] = self
+  end
+
+  def update(artist_name)
+    @artist_name = artist_name
+  end
+
+  def delete
+    @@artists.delete(@id)
+  end
+
+
+
+# class methods
+  @@artists = {}
+  @@total_rows = 0
+
+  def self.clear
+    @@artists = {}
+    @@total_rows = 0
+  end
+
+  def self.all
+    @@artists.values
+  end
+
+
+end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,4 +1,51 @@
 require 'rspec'
 require 'artist'
-require 'stage'
+require 'artist'
 require 'pry'
+
+describe 'Artist' do
+  before(:each) do
+    Artist.clear()
+    @artist1 = Artist.new(nil, "Michael Jackson", "Elm").save
+    @artist2 = Artist.new(nil, "Pavorati", "Oak").save
+    @artist3 = Artist.new(nil, "Kenny G", "Maple").save
+
+  end
+  describe ('.all') do
+    it ('returns an empty array when there are no artists')do
+      Artist.clear
+      expect(Artist.all).to(eq([]))
+    end
+  end
+
+  describe ('#save') do
+    it('saves a artist') do
+      artist4 = Artist.new(nil, "Queen").save
+      expect(Artist.all).to(eq([@artist1, @artist2, @artist3, artist4]))
+    end
+  end
+
+  describe ('#update') do
+    it('updates the name of the artist') do
+      @artist3.update("Michael Bolton")
+      expect(@artist3.artist_name).to(eq("Michael Bolton"))
+    end
+  end
+
+  describe ('#delete') do
+    it('deletes a artist from the hash') do
+      @artist3.delete
+      expect(Artist.all).to(eq([@artist1, @artist2]))
+    end
+  end
+  describe('.find_by_stage') do
+  it("finds artists for a stage") do
+    stage2 = Stage.new(nil, "Poplar").save
+    artist = Artist.new(nil, "Cake", @stage_name).save
+    artist2 = Artist.new(nil, "California", stage2.id).save
+    expect(Artist.find_by_stage(stage2.id)).to(eq([artist2]))
+  end
+end
+
+
+end
